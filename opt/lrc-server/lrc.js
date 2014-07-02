a4223a4922e37caceef555355444d3cdcba41cfa
Nicolas Hurtubise <316k@legtux.org>
2014-07-02 13:53:39 -0400

    Adds a generic music manager + a route for music Supported drivers : Rhythmbox, moc

--- opt/lrc-server/lrc.js
+++ opt/lrc-server/lrc.js
@@ -3,7 +3,57 @@ var express = require("express"),
         sys = require("sys"),
         exec = require("child_process").exec,
         child;
-        
+
+/**
+ * Currently supported drivers : music-on-console (aka moc) & rhythmbox
+ */
+function Music_Manager() {
+    this.drivers = {
+        rhythmbox: {
+            infos: "rhythmbox-client --print-playing-format='%ta;%at;%tt;%te;%td;'",
+            parse_infos: function(stdout) {
+                info = stdout.split(";");
+                return {artist: escape(info[0]), album: escape(info[1]), title: escape(info[2]), elapsed: info[3], duration: info[4]};
+            },
+            toggle_play: "export DISPLAY=:0; xdotool key XF86AudioPlay",
+            stop: "export DISPLAY=:0; xdotool key XF86AudioStop",
+            previous: "export DISPLAY=:0; xdotool key XF86AudioPrev",
+            next: "export DISPLAY=:0; xdotool key XF86AudioNext",
+            seek: null, // Doesn't seem to be possible with rhythmbox
+        },
+        moc: {
+            infos: "mocp -Q '%artist;%album;%song;%cs;%ts'",
+            parse_infos: function(stdout) {
+                info = stdout.split(";");
+                return {artist: escape(info[0]), album: escape(info[1]), title: escape(info[2]), elapsed: parseInt(info[3]), duration: parseInt(info[4])};
+            },
+            toggle_play: "mocp -G",
+            stop: "mocp -P",
+            previous: "mocp -r",
+            next: "mocp -f",
+            /**
+             * proportion is a float, between 0 and 1
+             */
+            seek: function(driver, args) {
+                var self = driver;
+
+                exec(self.infos, function(error, stdout, stderr) {
+                    var infos = self.parse_infos(stdout);
+                    var proportion = args.proportion || infos.elapsed/infos.duration;
+                    var seek_to = infos.duration * proportion;
+                    console.log(seek_to);
+                    exec("mocp -k " + parseInt(seek_to - infos.elapsed));
+                });
+            }
+        }
+    };
+    // Default driver
+    this.default_driver = 'rhythmbox';
+}
+
+var music_manager = new Music_Manager();
+
+// Relative mouse move uses WebSocket
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port: 3001});
 wss.on('connection', function(ws) {
@@ -13,6 +63,37 @@ wss.on('connection', function(ws) {
     });
 });
 
+// Route to handle music commands
+app.all("/music", function(req, res) {
+    var driver_name = req.query.driver;
+
+    if(!(driver_name in music_manager.drivers)) {
+        driver_name = music_manager.default_driver;
+    }
+
+    var driver  = music_manager.drivers[driver_name];
+
+    if('info' in req.query) {
+        exec(driver.infos, function(error, stdout, stderr) {
+            var infos = driver.parse_infos(stdout);
+            res.send(infos);
+        });
+    } else if('action' in req.query && req.query.action in driver) {
+        var command = driver[req.query.action];
+
+        if(typeof command == 'string') {
+            exec(command);
+        } else if(typeof command == 'function') {
+            command(driver, JSON.parse(req.query.args) || {});
+        } else {
+            res.send({error: "command not supported by driver " + driver_name});
+        }
+    } else {
+        res.send({error: "undefined action for driver " + driver_name});
+    }
+});
+
+// Handles arbitrary commands sent from lrc-client
 app.all("/lrc", function(req, res) {
     var command = req.query.cmd;
     exec(command, function(err, stdout, stderr) {
@@ -53,7 +134,6 @@ app.get(/^\/(.*)/, function(req, res) {
             }
         }
     });
-
 });
 
 app.listen(3000, function () {
