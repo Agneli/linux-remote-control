48c3ffabacc232e9bc9c44cd01008507115fa1dc
Nicolas Hurtubise <316k@legtux.org>
2014-09-23 13:11:57 -0400

    Moves configuration.js to to server root

--- opt/lrc-server/configuration.js
+++ opt/lrc-server/configuration.js
@@ -2,12 +2,12 @@ exports.config = {
     // Available drivers : rhythmbox, moc
     music_driver: "rhythmbox",
     port: 3000,
-    websocket_port: 3001,
     // Mouse speed multiplicator
     mouse_speed: {
         x: 2,
         y: 2
     },
+    // Available drivers : HTTP, WebSocket
     connection_driver: 'HTTP',
     // Indicates if the client app is installed as a certified app
     certified: false
