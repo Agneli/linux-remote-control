1f68c134d4d3189114a7ea95e9c1a6ee69ed46c4
Nicolas Hurtubise <316k@legtux.org>
2014-09-25 01:38:04 -0400

    Adds generic actions and connection drivers The same actions can now be performed via HTTP and WebSocket requests

--- opt/lrc-server/lrc.js
+++ opt/lrc-server/lrc.js
@@ -1,73 +1,40 @@
-//CONSTANTS
-var CLICK_PREFIX = 'c';
-var MOVE_PREFIX = 'm';
-var SCROLL_PREFIX = 's';
-
-var express = require("express"),
-        app = express(),
+// Required stuff
+var     app = require("express")(),
         sys = require("sys"),
         exec = require("child_process").exec,
-        child = null,
         config = require("./configuration.js").config,
         music_manager = require("music.js").drivers[config.music_driver],
         cmd = require("cmd.js"),
-        child;
+        connection = require("connection.js").drivers[config.connection_driver],
+        WebSocketServer = require('ws').Server;
 
-// Relative mouse move uses WebSocket
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({port: config.websocket_port});
-var values, x, y;
-var handleMessage = function(message) {
-    var prefix = message[0];
-    message = message.substr(1);
-    switch (prefix) {
-        case CLICK_PREFIX:
-            //Clicks are not yet sent over websocket
-        break;
-        case MOVE_PREFIX:
-            values = message.split(';');
-            x = parseInt(values[0]) * config.mouse_speed.x;
-            y = parseInt(values[1]) * config.mouse_speed.y;
-            if (Math.abs(x) > 10) {
-                if (Math.abs(x) > 20) {
-                    x = x * 2;
-                }
-                x = x * 2;
+var actions = {
+    info: function(parameters, callback) {
+        exec("amixer sget Master | grep '%]' && xbacklight -get", function(error, stdout, stderr) {
+            var volume = stdout.split("%]");
+            volume = volume[0].split("[");
+            volume = volume[1];
+
+            var backlight = stdout.split(/\[o(?:n|ff)\]/); // Matches [on] or [off]
+
+            // Unmute the speakers if necessary
+            if(stdout.indexOf("[off]") != -1) {
+                exec("amixer sset Master unmute");
             }
-            if (Math.abs(y) > 10) {
-                if (Math.abs(y) > 20) {
-                    y = y * 2;
-                }
-                y = y * 2;
-            }
-            exec('xdotool mousemove_relative -- ' + x + ' ' + y, function puts(error, stdout, stderr) {});
-        break;
-        case SCROLL_PREFIX:
-            var button = message < 0 ? 4 : 5;
-            exec('xdotool click ' + button, function puts(error, stdout, stderr) {});
-        break;
-        default:
-            console.log('WebSocket received : ' + message);
-        break;
-    }
-};
-wss.on('connection', function(ws) {
-    ws.on('message', handleMessage);
-    setInterval(function() {
-        wss.broadcast(JSON.stringify({status: 'Running', driver: config.connection_driver, port: config.port}));
-    }, 3000);
-});
 
-// WebSocket test
-wss.broadcast = function(data) {
-    for(var i in this.clients) {
-        this.clients[i].send(data);
-    }
-};
+            backlight = backlight[backlight.length-1].trim();
+            backlight = backlight.split(".");
+            backlight = backlight[0] || 0;
 
-// Route to handle music commands
-app.all("/music", function(req, res) {
-    if ('info' in req.query) {
+            var data = {
+                volume: volume,
+                backlight: backlight,
+            };
+
+            callback(data);
+        });
+    },
+    music_info: function(parameters, callback) {
         exec(music_manager.infos, function(error, stdout, stderr) {
             var infos = music_manager.parse_infos(stdout);
             if(!isNaN(infos.elapsed) && !isNaN(infos.duration)) {
@@ -75,83 +42,93 @@ app.all("/music", function(req, res) {
                 infos["duration-formatted"] = Math.floor(infos.duration/60) + ':' + (infos.duration % 60 < 10 ? '0' : '') + (infos.duration % 60);
                 infos["elapsed-percent"] = infos.elapsed/infos.duration*100;
             }
-            res.send(req.query.callback + '(' + JSON.stringify({music: infos}) + ')');
+            callback({music: infos});
         });
-    } else if ('action' in req.query && req.query.action in music_manager) {
-        var command = music_manager[req.query.action];
+    },
+    music: function(parameters, callback) {
+        if ('action' in parameters && parameters.action in music_manager) {
+            var command = music_manager[parameters.action];
 
-        if (typeof command == 'string') {
-            exec(command);
-            res.send({state: 0});
-        } else if (typeof command == 'function') {
-            command(music_manager, exec, req.query.args || {});
-            res.send({state: 0});
+            if (typeof command == 'string') {
+                exec(command);
+                callback(1)
+            } else if (typeof command == 'function') {
+                command(music_manager, exec, parameters.args || {});
+                callback(1);
+            } else {
+                callback({error: "command not supported by driver " + music_manager.name});
+            }
         } else {
-            res.send({state: 1, error: "command not supported by driver " + music_manager.name});
+            callback({error: "undefined action for driver " + music_manager.name});
         }
-    } else {
-        res.send({state: 1, error: "undefined action for driver " + music_manager.name});
-    }
-});
-
-// Handles arbitrary commands sent from lrc-client
-app.all("/lrc", function(req, res) {
-    var command = req.query.cmd;
-    exec(command, function(err, stdout, stderr) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.send({stdout: stdout, error: err, stderr: stderr});
-    });
-});
-
-app.all("/info", function(req, res) {
-    child = exec("amixer sget Master | grep '%]' && xbacklight -get", function(error, stdout, stderr) {
-        res.header("Content-Type", "text/javascript");
-        var volume = stdout.split("%]");
-        volume = volume[0].split("[");
-        volume = volume[1];
-
-        var backlight = stdout.split(/\[o(?:n|ff)\]/); // Matches [on] or [off]
-
-        // Unmute the speakers
-        if(stdout.indexOf("[off]") != -1) {
-            exec("amixer sset Master unmute");
+    },
+    // Execute an arbitrary command line
+    lrc: function(parameters, callback) {
+        var command = parameters.cmd || '';
+        exec(command, function(err, stdout, stderr) {
+            callback({stdout: stdout, error: err, stderr: stderr});
+        });
+    },
+    /*
+     * Mouse click, short prefix for performence issues
+     * parameters.b: {1: left click, 2: middle click, 3: right click}
+     */
+    c: function(parameters, callback) {
+        exec('xdotool click ' + parameters.b, new Function);
+        callback(1);
+    },
+    // Mouse move
+    m: function(parameters, callback) {
+        x = parseInt(parameters.x) * config.mouse_speed.x;
+        y = parseInt(parameters.y) * config.mouse_speed.y;
+        if (Math.abs(x) > 10) {
+            if (Math.abs(x) > 20) {
+                x = x * 2;
+            }
+            x = x * 2;
         }
+        if (Math.abs(y) > 10) {
+            if (Math.abs(y) > 20) {
+                y = y * 2;
+            }
+            y = y * 2;
+        }
+        exec('xdotool mousemove_relative -- ' + x + ' ' + y, new Function);
+        callback(null);
+    },
+    // Mouse scroll
+    s: function(parameters, callback) {
+        var button = parameters.s < 0 ? 4 : 5;
+        exec('xdotool click ' + button, new Function);
+        callback(null);
+    },
+};
 
-        backlight = backlight[backlight.length-1].trim();
-        backlight = backlight.split(".");
-        backlight = backlight[0] || 0;
-
-        var data = {
-            volume: volume,
-            backlight: backlight,
-        };
-
-        res.send(req.query.callback + '(' + JSON.stringify(data) + ')');
-    });
-});
+// Init the connection with the right server object
+var servers = {
+    HTTP: app,
+    WebSocket: WebSocketServer,
+    Bluetooth: null
+};
+connection(servers, actions, config);
 
 /**
  * Handles all other requests by sending informations about the server
  */
-app.get(/^\/(.*)/, function(req, res) {
+app.get(/.*/, function(req, res) {
     res.header("Content-Type", "text/javascript");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.send({status: 'Running', driver: config.connection_driver, port: config.port});
 });
 
-app.listen(config.port, function () {
-    console.log('Listening on port ' + config.port);
+/**
+ * If certified mode is enabled, we can use the command line as an
+ * interpreter manage SMS on the client device
+ */
+if(config.certified) {
+    console.log('> SMS command line, type `help` for more information.');
 
-    /**
-     * If certified mode is enabled, we can use the command line as an
-     * interpreter manage SMS on the client device
-     */
-    if(config.certified) {
-        console.log('> SMS command line, type `help` for more information.');
-
-        // Command line listener
-        process.openStdin().addListener("data", cmd.parse_cmd);
-    }
-});
+    // Command line listener
+    process.openStdin().addListener("data", cmd.parse_cmd);
+}
